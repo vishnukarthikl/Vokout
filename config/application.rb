51b7fcad8dfa43a20d2e92f3b40044021a53e812
Vishnu karthik <vishnucarbon@gmail.com>
2015-03-12 22:07:37 +0530

    using erb to get asset path

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module Vorkout
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 
-    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|html)$)
   end
 end
