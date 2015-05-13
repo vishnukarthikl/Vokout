abc78ea66ea916133f0ccc74da295d75af822793
Vishnu karthik <vishnucarbon@gmail.com>
2015-05-13 21:56:29 +0530

    changing timezone to local

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,10 @@ module Vorkout
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+      config.time_zone = 'Chennai'
+      config.active_record.default_timezone = :local
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+      # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
