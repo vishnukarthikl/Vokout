5af05a3d08aeb125adff38f321fb286bf28b1393
Vishnu karthik <vishnucarbon@gmail.com>
2015-05-26 11:10:29 +0530

    fixing dependent destroy

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,7 +4,7 @@ class Subscription < ActiveRecord::Base
 
   belongs_to :member
   belongs_to :membership
-  has_one :revenue, as: :purchasable, dependent: destroy
+  has_one :revenue, as: :purchasable, dependent: :destroy
 
   def end_date
     if self.extended_till
